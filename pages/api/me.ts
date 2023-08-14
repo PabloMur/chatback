b93e76cf835a6d4682420c5aca8b2c6cf913a82a
PabloMur <polillomurillo@gmail.com>
2023-08-14 13:26:13 -0300

    endpoint funcionando correctamente

--- pages/api/me.ts
+++ pages/api/me.ts
@@ -14,7 +14,7 @@ export default async function handler(
     });
     if (req.method === "POST") {
       const userData = await userController.getUserData(req, res);
-      res.status(200).json({ test: userData });
+      res.status(200).json({ userData });
     } else {
       res.status(405).json({ error: "Method Not Allowed" });
     }
