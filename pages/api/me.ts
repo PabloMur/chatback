bc0bb1919ccb064c48373d8362285e6d0ebd9596
PabloMur <polillomurillo@gmail.com>
2023-08-14 12:33:55 -0300

    asd

--- pages/api/me.ts
+++ pages/api/me.ts
@@ -1,14 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import userController from "../../controllers/userController";
+import NextCors from "nextjs-cors";
+//import userController from "../../controllers/userController";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
-    const userData = await userController.getUserData(req, res);
-    res.status(200).json({ test: userData });
-  } else {
-    res.status(405).json({ error: "Method Not Allowed" });
+  try {
+    await NextCors(req, res, {
+      methods: ["POST"],
+      origin: "*",
+      optionsSuccessStatus: 200,
+    });
+    if (req.method === "POST") {
+      //const userData = await userController.getUserData(req, res);
+      res.status(200).json({ test: "TEST" });
+    } else {
+      res.status(405).json({ error: "Method Not Allowed" });
+    }
+  } catch (error) {
+    console.error(error);
   }
 }
