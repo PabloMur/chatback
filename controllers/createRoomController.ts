2506ea4713d2139604059d93319321c7509623ea
PabloMur <polillomurillo@gmail.com>
2023-08-15 01:59:38 -0300

    delete en fincionamiento

--- controllers/createRoomController.ts
+++ controllers/createRoomController.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import CreateChatroomModel from "../models/ChatroomModel";
+import ChatroomModel from "../models/RoomModel";
 
 class CreateChatroomController {
   static async createChatroom(req: NextApiRequest, res: NextApiResponse) {
@@ -14,6 +15,24 @@ class CreateChatroomController {
     try {
       const roomId = await CreateChatroomModel.createChatroom(token);
       res.status(200).json({ roomId });
+    } catch (error) {
+      res.status(401).json({ error: error });
+    }
+  }
+
+  static async deleteRoom(req: NextApiRequest, res: NextApiResponse) {
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "");
+    const { roomId } = req.query as any;
+
+    if (!token) {
+      res.status(401).json({ error: "Missing authorization token" });
+      return;
+    }
+
+    try {
+      const deletRoom = await ChatroomModel.deleteRoom(roomId);
+      return deletRoom;
     } catch (error) {
       res.status(401).json({ error: error });
     }
